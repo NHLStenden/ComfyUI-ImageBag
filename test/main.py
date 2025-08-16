55ebd63d7f60bb88462db94d767d34d67e7c246a
martin-bosgra-nhl-stenden <martin.bosgra@nhlstenden.com>
2025-08-16 08:27:44 +0200

    Added test script

---
+++ test/main.py
@@ -0,0 +1,8 @@
+from imagebag.enhanced_image_colour_transfer_node import EnhancedImageColourTransferNode
+
+def main():
+    e = EnhancedImageColourTransferNode()
+    print("Execution complete.")
+
+if __name__ == "__main__":
+    main()
